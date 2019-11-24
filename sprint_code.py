449d3094f511edcd4795cc2353afd661f29604d4
oded kaplan <odedkaplan40@gmail.com>
2019-11-24 19:00:52 +0200

    fixed bugs in image processing

--- sprint_code.py
+++ sprint_code.py
@@ -3,7 +3,7 @@ import imutils as imutils
 import numpy as np
 
 #test
-
+objects = 3
 cap = cv2.VideoCapture(1)
 
 # Creating a window for later use
@@ -34,12 +34,12 @@ def get_center(hsv, lower, upper):
     cnts.sort(reverse=True, key=cv2.contourArea)
     if len(cnts) == 0:
         return 0, 0
-    c = cnts[0]
+    c = cnts[0:min([objects, len(cnts)])]
 
     return c, result
 
 
-lower_blue = np.array([60, 50, 200])
+lower_blue = np.array([60, 50, 120])
 upper_blue = np.array([160, 150, 255])
 
 x_array = []
@@ -47,6 +47,7 @@ y_array = []
 
 while True:
     _, frame = cap.read()
+    print("frame")
     cv2.imshow("Original", frame)
     # converting to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -60,17 +61,17 @@ while True:
         if k == 27:
             break
         continue
-    elif not (car is 0):
-        print("cant find car")
-        to_draw_car = False
-        c = car
-        M = cv2.moments(c)
-        if M["m00"] != 0:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            centers.append((cX, cY))
+    # elif not (car is 0):
+    #     print("cant find car")
+    #     to_draw_car = False
+    #     c = car
+    #     M = cv2.moments(c)
+    #     if M["m00"] != 0:
+    #         cX = int(M["m10"] / M["m00"])
+    #         cY = int(M["m01"] / M["m00"])
+    #         centers.append((cX, cY))
     else:
-        for c in [car]:
+        for c in car:
             # compute the center of the contour
             M = cv2.moments(c)
             if M["m00"] != 0:
@@ -78,21 +79,15 @@ while True:
                 cY = int(M["m01"] / M["m00"])
                 centers.append((cX, cY))
 
-    if len(centers) < 2:
+    if len(centers) < 1:
         continue
-    cX, cY = centers[0]
-    x_array.append(cX)
-    y_array.append(cY)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    x_array.append(cX)
-    y_array.append(cY)
-
-    cX, cY = centers[1]
-    cv2.drawContours(result, [car], -1, (0, 255, 0), 2)
-    cv2.circle(result, (cX, cY), 7, (255, 255, 255), -1)
-    cv2.putText(result, "car", (cX - 20, cY - 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    for i in range(len(centers)):
+        cX, cY = centers[i]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.drawContours(result, [car[i]], -1, (0, 255, 0), 2)
+        cv2.circle(result, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(result, "car", (cX - 20, cY - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     cv2.imshow('result', result)
 
     k = cv2.waitKey(5) & 0xFF
