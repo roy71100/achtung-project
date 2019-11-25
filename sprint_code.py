bdfd61c604ea010c622ff4605593f829bd0f1072
oded kaplan <odedkaplan40@gmail.com>
2019-11-26 01:03:07 +0200

    sprint_code.py is so much more usable and modular
    finished measuring location of red object in board with green borders
    
    added trackbars.py which enables calibrating hsl boundaries easily

--- sprint_code.py
+++ sprint_code.py
@@ -1,10 +1,11 @@
 import cv2
 import imutils as imutils
 import numpy as np
-import test_rotation
+import image_rotation
 
 # test
-objects = 4
+green_objects = 4
+red_objects = 1
 cap = cv2.VideoCapture(1)
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 # Creating a window for later use
@@ -14,16 +15,20 @@ cv2.namedWindow('result')
 def process_frame(frame, lower, upper):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper)
+    # except:
+    #     print("hsv")
+    #     mask = cv2.inRange(frame, lower, upper)
 
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
     gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
     # blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow("thresh", thresh)
     return result, thresh
 
 
-def get_contours(thresh):
+def get_contours(thresh, objects_count):
     """
     """
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
@@ -33,8 +38,8 @@ def get_contours(thresh):
     cnts.sort(reverse=True, key=cv2.contourArea)
     if len(cnts) == 0:
         return 0, 0
-    c = cnts[0:min([objects, len(cnts)])]
-    return c, result
+    c = cnts[0:min([objects_count, len(cnts)])]
+    return c
 
 
 def calc_centers(contours):
@@ -49,47 +54,81 @@ def calc_centers(contours):
     return centers
 
 
-def draw_centers(centers, car):
+def draw_centers(centers, text, contours, pic):
     for i in range(len(centers)):
         cX, cY = centers[i]
-        cv2.drawContours(result, [car[i]], -1, (0, 255, 0), 2)
-        cv2.circle(result, (cX, cY), 7, (255, 255, 255), -1)
-        cv2.putText(result, "car", (cX - 20, cY - 20),
+        cv2.drawContours(pic, [contours[i]], -1, (0, 255, 0), 2)
+        cv2.circle(pic, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(pic, text, (cX - 20, cY - 20),
                     FONT, 0.5, (255, 255, 255), 2)
     # if len(centers) >= 2:
     #     cv2.rectangle(result, centers[0], centers[1], (0, 0, 255), 2)
 
-lower_blue = np.array([60, 50, 40])
-upper_blue = np.array([160, 150, 255])
 
-while True:
-    _, frame = cap.read()
-    cv2.imshow("Original", frame)
-    # converting to HSV
-    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    result, post_threshold = process_frame(frame, lower_blue, upper_blue)
-    car, result = get_contours(post_threshold)
+def get_color_contours(frame, lower, upper, color):
+    result, post_threshold = process_frame(frame, lower, upper)
+    if color == "green":
+        color_contours = get_contours(post_threshold, green_objects)
+    elif color == "red":
+        color_contours = get_contours(post_threshold, red_objects)
+    return color_contours
 
-    if car is 0:
+
+def calc_corners(corners, frame):
+    if corners is 0:
         cv2.imshow('result', frame)
-        k = cv2.waitKey(5) & 0xFF
-        if k == 27:
-            break
-        continue
+        return 0
 
     else:
-        centers = calc_centers(car)
+        centers = calc_centers(corners)
 
     if len(centers) == 0:
-        continue
+        return 0
+
+    draw_centers(centers, "corner", corners, frame)
 
     if len(centers) >= 4:
         points = np.array([np.array(centers[i]) for i in range(4)])
-        warped = test_rotation.four_point_transform(frame, points)
-        cv2.imshow('warped', warped)
-    draw_centers(centers, car)
-    cv2.imshow('result', result)
+        warp = image_rotation.four_point_transform(frame, points)
+        return warp
+    return 1
 
+
+lower_green = np.array([60, 90, 70])
+upper_green = np.array([170, 180, 255])
+
+lower_red = np.array([0, 205, 90])
+upper_red = np.array([50, 255, 150])
+counter = 0
+while True:
+    counter += 1
+    _, frame = cap.read()
+    cv2.imshow("Original", frame)
+
+    corner_contours = get_color_contours(frame, lower_green, upper_green, "green")
+
+    warped = calc_corners(corner_contours, frame)
+
+    if warped is 0:
+        continue
+
+    if not (warped is 1):
+        red_contours = get_color_contours(warped, lower_red, upper_red, "red")
+
+        red_centers = calc_centers(red_contours)
+
+        draw_centers(red_centers, "PLAYER 1", red_contours, warped)
+
+        if counter % 50 == 0:
+            shape = warped.shape
+            try:
+                print(red_centers[0][0] / shape[1], red_centers[0][1] / shape[0])
+                # print(red_centers[0][0], red_centers[0][1])
+                # print(shape)
+            except:
+                pass
+    cv2.imshow('warped', warped)
+    cv2.imshow('result', frame)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
